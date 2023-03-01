e7e263b5bca12a382212766d7725ddc58362330b
Roman Koff <krv@aanet.ru>
2023-03-01 12:20:40 +0300

    fix 1

--- dist/make-list.py
+++ dist/make-list.py
@@ -5,8 +5,11 @@ with open('style.css', 'r', encoding='utf8') as f1:
         if s1.startswith('.gi-'):
             s2 = s1.split(':', 1)[0][1:]
         if s1.startswith('  content:'):
-            items.append(s2 + '=' + s1.split('"')[1][1:])
+            s3 = s1.split('"')[1][1:]
+            items.append(f'"{s2}": "{s3}"')
 
-with open('icons.txt', 'w', encoding='utf8') as f1:
+with open('guap-icons.json', 'w', encoding='utf8') as f1:
+    f1.write('{\n')
     for s1 in items:
-        f1.write(f'"{s1}",\n')
+        f1.write(f'{s1},\n')
+    f1.write('}\n')
